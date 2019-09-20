7e269eee32412e2579401aa1ce034b4baed4010c
chanrl <richard129chan@gmail.com>
2019-09-19 22:03:21 -0700

    Update

--- web.py
+++ web.py
@@ -6,10 +6,7 @@ from selenium.webdriver.support.select import Select
 import selenium.webdriver.support.ui as ui
 import selenium.webdriver.support.expected_conditions as EC
 import pandas as pd
-import os
 import time
-import glob
-import re
 
 # page = pd.read_html('http://www.sportsplays.com/consensus/all.html')
 
@@ -23,41 +20,30 @@ class scrape():
     self.driver = webdriver.Chrome(options=options)
 
   def oddsshark(self,team=""):
+    '''
+    Parameters
+    ----------
+    team = str of team e.g.'Atlanta'
+    '''
     self.driver.get('https://www.oddsshark.com/nfl/database')
     ui.WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.ID, "team-search-h2h")))
     self.driver.find_element_by_id("team-search-h2h").send_keys(team)
-    try:
-      self.driver.find_element_by_id("games-30-h2h").click()
-    except:
-      print("games-30 click doesn't work")
-    try:
-      dataset_drop_down_element = ui.WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.ID, 'chalk-select-game-type-h2h')))
-      dataset_drop_down_element = Select(dataset_drop_down_element)
-      #dataset_drop_down_element.select_by_visible_text('Regular Season')
-      dataset_drop_down_element.select_by_index(1) 
-    except:
-      print("Played in drop down doesn't work")
-    try:  
-      self.driver.find_element_by_id("location-any-h2h").click()
-    except:
-      print("Anywhere button doesn't work")
-    try:
-      dataset_drop_down_element = ui.WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.ID, 'chalk-select-odds-h2h')))
-      dataset_drop_down_element = Select(dataset_drop_down_element)
-      dataset_drop_down_element.select_by_visible_text('Either')
-    except:
-      print("favorite/underdog dropdown doesn't work")
-    try:
-      self.driver.find_element_by_id("submit-h2h").click() #working
-    except:
-      print("search button doesn't work")
-    #page = driver.page_source
+    self.driver.find_element_by_xpath('//*[@id="frm-h2h"]/table/tbody/tr[3]/td/div/div[3]/label').click()
+    self.driver.find_element_by_xpath('//*[@id="frm-h2h"]/table/tbody/tr[6]/td/div/div[1]/label').click()
+    self.driver.execute_script('document.getElementById("chalk-select-game-type-h2h").value = "REG/PST"')
+    self.driver.execute_script('document.getElementById("chalk-select-odds-h2h").value = "ANY"')
+    time.sleep(3)
+    self.driver.find_element_by_xpath('//*[@id="submit-h2h"]').click()
+    time.sleep(3)
+    self.page = self.driver.page_source
 
   def sportsplays(self, month, day, year):
     '''
     Parameters
     ----------
     month - str of month e.g December
+    day - int 2 digits e.g. 01
+    year - int 4 digits e.g. 2019
     '''
     self.driver.get('http://www.sportsplays.com/consensus/all.html')
     dataset_drop_down_element = ui.WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.ID, 'from_date_month')))
